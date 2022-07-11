667ac6a19768616afd6a0d81791f5a58574de4ea
Eglany Junior <contas.eglanyjr@gmail.com>
2022-07-11 17:35:01 -0400

    :sparkles: model interface

---
+++ src/interfaces/IModel.ts
@@ -0,0 +1,9 @@
+interface Model<T> {
+  create({ item }: { item: T }): Promise<T>;
+  read(item: T[]): Promise<T>;
+  readOne(item: string): Promise<T> | null;
+  update(itemA: string, { itemB }: { itemB:T }): Promise<T> | null;
+  delete(itemA: string, { itemB }: { itemB:T }): Promise<T> | null;
+}
+
+export default Model;

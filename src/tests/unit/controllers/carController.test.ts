f1bce326ac750fc531f8434954c1b1567ecf792d
Eglany Junior <contas.eglanyjr@gmail.com>
2022-07-13 23:46:03 -0400

    :white_check_mark: controllertest read

--- src/tests/unit/controllers/carController.test.ts
+++ src/tests/unit/controllers/carController.test.ts
@@ -21,6 +21,24 @@ const carMock = {
 	seatsQty: 5
 }
 
+const carsMock = [{
+	model: 'cruze',
+	year: 2015,
+	color: 'gray',
+	status: false,
+	buyValue: 80000,
+	doorsQty: 4,
+	seatsQty: 5
+},
+{
+	model: 'fusca',
+	year: 1998,
+	color: 'red',
+	status: false,
+	buyValue: 20000,
+	doorsQty: 2,
+	seatsQty: 2
+}]
 
 describe("Test CarController", () => {
   let serviceStub: Sinon.SinonStub;
@@ -50,6 +68,34 @@ describe("Test CarController", () => {
       expect(response.body).to.have.property("buyValue");
       expect(response.body).to.have.property("doorsQty");
       expect(response.body).to.have.property("seatsQty");
+    });
+  });
+
+  describe("method read", () => {
+    before(() => {
+      serviceStub = Sinon.stub(CarService.prototype, "read");
+      serviceStub.resolves(carsMock);
+    });
+
+    after(() => {
+      serviceStub.restore();
+    });
+
+    it("successfully find all car", async () => {
+      const response = await chai
+        .request(server.app)
+        .get(`${carController.route}`);
+
+      expect(response).to.have.status(200);
+      expect(response.body).to.be.an("array");
+      expect(response.body[0]).to.be.an("object");
+
+      expect(response.body[0]).to.have.property("model");
+      expect(response.body[0]).to.have.property("year");
+      expect(response.body[0]).to.have.property("color");
+      expect(response.body[0]).to.have.property("buyValue");
+      expect(response.body[0]).to.have.property("doorsQty");
+      expect(response.body[0]).to.have.property("seatsQty");
     });
   });
 })

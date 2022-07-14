92f01d29f5cbfa362f9e6fcfee441fc4bab86234
Eglany Junior <contas.eglanyjr@gmail.com>
2022-07-13 23:41:39 -0400

    :white_check_mark: model test read

--- src/tests/unit/models/carModel.test.ts
+++ src/tests/unit/models/carModel.test.ts
@@ -16,6 +16,26 @@ const carMock = {
 	seatsQty: 5
 }
 
+const carsMock = [{
+	model: 'cruze',
+	year: 2015,
+	color: 'gray',
+	status: false,
+	buyValue: 80000,
+	doorsQty: 4,
+	seatsQty: 5
+},
+{
+	model: 'fusca',
+	year: 1998,
+	color: 'red',
+	status: false,
+	buyValue: 20000,
+	doorsQty: 2,
+	seatsQty: 2
+}
+]
+
 describe("Test CarModel", () => {
   let modelStub: Sinon.SinonStub;
   // let isValidIdStub: Sinon.SinonStub;
@@ -30,7 +50,7 @@ describe("Test CarModel", () => {
       modelStub.restore();
     });
 
-    it("uccessfully created car", async () => {
+    it("successfully created car", async () => {
       const createdCar = await carModel.create(carMock);
 
       expect(createdCar).to.be.an("object");
@@ -40,6 +60,31 @@ describe("Test CarModel", () => {
       expect(createdCar).to.have.property("buyValue");
       expect(createdCar).to.have.property("doorsQty");
       expect(createdCar).to.have.property("seatsQty");
+    });
+  });
+
+  describe("Test method .read", () => {
+    before(() => {
+      modelStub = Sinon.stub(Mongoose.Model, "find");
+      modelStub.resolves(carsMock);
+    });
+
+    after(() => {
+      modelStub.restore();
+    });
+
+    it("successfully find all car", async () => {
+      const foundCars = await carModel.read();
+
+      expect(foundCars).to.be.an("array");
+      expect(foundCars[0]).to.be.an("object");
+
+      expect(foundCars[0]).to.have.property("model");
+      expect(foundCars[0]).to.have.property("year");
+      expect(foundCars[0]).to.have.property("color");
+      expect(foundCars[0]).to.have.property("buyValue");
+      expect(foundCars[0]).to.have.property("doorsQty");
+      expect(foundCars[0]).to.have.property("seatsQty");
     });
   });
 })

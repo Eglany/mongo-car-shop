e62a29188e7619d77cd25d67abfd60926ab2c0b7
Eglany Junior <contas.eglanyjr@gmail.com>
2022-07-13 23:35:34 -0400

    :white_check_mark: controller test create

--- src/tests/unit/controllers/carController.test.ts
+++ src/tests/unit/controllers/carController.test.ts
@@ -1,18 +1,55 @@
-// import chai, { expect } from "chai";
-// import chaiHttp from "chai-http";
-// import Sinon from "sinon";
+import chai, { expect } from "chai";
+import chaiHttp from "chai-http";
+import Sinon from "sinon";
+import CarController from "../../../controllers/CarController";
+import server from "../../../server";
+import CarService from "../../../services/CarService";
 
-// import CarService from "../../../services/CarService";
-// import CarController from "../../../controllers/CarController";
 
-// import server from "../../../server";
 
-// chai.use(chaiHttp);
+chai.use(chaiHttp);
 
-// const carController = new CarController();
+const carController = new CarController();
 
-// const FAKE_MONGO_ID = "5e8f8f8f8f8f8f8f8f8f8f8f";
+const carMock = {
+	model: 'cruze',
+	year: 2015,
+	color: 'gray',
+	status: false,
+	buyValue: 80000,
+	doorsQty: 4,
+	seatsQty: 5
+}
 
-// describe("Test CarController", () => {
-//   let serviceStub: Sinon.SinonStub;
-// })
+
+describe("Test CarController", () => {
+  let serviceStub: Sinon.SinonStub;
+
+  describe("method create", () => {
+    before(() => {
+      serviceStub = Sinon.stub(CarService.prototype, "create");
+      serviceStub.resolves(carMock);
+    });
+
+    after(() => {
+      serviceStub.restore();
+    });
+
+    it("successfully created car", async () => {
+      const response = await chai
+        .request(server.app)
+        .post(`${carController.route}`)
+        .send(carMock);
+
+      expect(response).to.have.status(201);
+      expect(response.body).to.be.an("object");
+
+      expect(response.body).to.have.property("model");
+      expect(response.body).to.have.property("year");
+      expect(response.body).to.have.property("color");
+      expect(response.body).to.have.property("buyValue");
+      expect(response.body).to.have.property("doorsQty");
+      expect(response.body).to.have.property("seatsQty");
+    });
+  });
+})
